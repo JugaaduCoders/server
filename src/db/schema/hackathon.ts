c9cf82bd1ee709d17747ef06e2ffad38682d8610
divanshu3451 <awesomedev3451@gmail.com>
2024-12-19 20:16:22 +0530

    fix: added one to many relation using relations function

--- src/db/schema/hackathon.ts
+++ src/db/schema/hackathon.ts
@@ -8,11 +8,12 @@ import {
 } from "drizzle-orm/pg-core";
 import { user } from "./user";
 import { timestamps } from "./schema.helper";
+import { relations } from "drizzle-orm";
 
 export const hackathon = pgTable("Hackathons", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  createdBy: integer().references(() => user.id),
-  name: varchar().notNull(),
+  createdBy: integer(),
+  name: varchar({ length: 255 }).notNull(),
   registrationDeadline: date().notNull(),
   startDate: date().notNull(),
   endDate: date().notNull(),
@@ -20,3 +21,10 @@ export const hackathon = pgTable("Hackathons", {
   maxTeamSize: smallint(),
   ...timestamps,
 });
+
+export const hackathonRelations = relations(hackathon, ({ one }) => ({
+  user: one(user, {
+    fields: [hackathon.createdBy],
+    references: [user.id],
+  }),
+}));
