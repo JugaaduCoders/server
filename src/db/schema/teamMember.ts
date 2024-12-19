4071a6dfff2bcd36a2ae6f0535cf0cb2bccadefc
divanshu3451 <awesomedev3451@gmail.com>
2024-12-19 21:28:45 +0530

    feat: created team and team members table with many to many mapping with user table

---
+++ src/db/schema/teamMember.ts
@@ -0,0 +1,26 @@
+import { integer, pgTable, primaryKey } from "drizzle-orm/pg-core";
+import { team } from "./team";
+import { user } from "./user";
+import { relations } from "drizzle-orm";
+
+export const teamMember = pgTable(
+  "TeamMembers",
+  {
+    teamId: integer().references(() => team.id),
+    userId: integer().references(() => user.id),
+  },
+  (teamMember) => ({
+    primaryKey: primaryKey({ columns: [teamMember.userId, teamMember.teamId] }),
+  })
+);
+
+export const teamMemberRelations = relations(teamMember, ({ one }) => ({
+  team: one(team, {
+    fields: [teamMember.teamId],
+    references: [team.id],
+  }),
+  user: one(user, {
+    fields: [teamMember.userId],
+    references: [user.id],
+  }),
+}));
