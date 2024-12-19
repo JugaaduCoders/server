2b98d6c2d11c48afc7ed8bc637444a1e67d572c0
divanshu3451 <awesomedev3451@gmail.com>
2024-12-19 21:28:45 +0530

    feat: created team and team members table with many to many mapping with user table

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -3,6 +3,7 @@ import { timestamps } from "./schema.helper";
 import { relations } from "drizzle-orm";
 import { hackathon } from "./hackathon";
 import { hackathonParticipant } from "./hackathonParticipant";
+import { teamMember } from "./teamMember";
 
 export const rolesEnum = pgEnum("roles", ["participant", "user", "admin"]);
 
@@ -18,4 +19,5 @@ export const user = pgTable("Users", {
 export const userRelations = relations(user, ({ many }) => ({
   hackathon: many(hackathon),
   hackathonParticipant: many(hackathonParticipant),
+  teamMember: many(teamMember),
 }));
