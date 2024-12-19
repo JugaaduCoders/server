78062cd50b45f429e45805c353c5d39b661d3b1a
Prakash kumar Rathour <134403521+PrakashGit205@users.noreply.github.com>
2024-12-19 11:05:46 +0530

    Create .prettierrc.ts

---
+++ .prettierrc.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    printWidth: 100,
+    plugins: [require('prettier-plugin-prisma')],
+};
