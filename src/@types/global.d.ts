d4a96961e98e754dc66200a0bc7ffafcef4a8e19
Prakash Rathour <prakashkumarrathour247@gmail.com>
2024-12-21 11:47:00 +0530

    feat: implements tokens error handling and routes

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -8,4 +8,5 @@ declare global {
     role: UserRole;
   }
 }
+
 export {};
---
+++ src/utils/jwt/index.ts
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { ErrorType } from "../../types/types";
+import { serverLog } from "../logs";
+
+export function generateToken(user: User) {
+  const payload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+  const options = {
+    expiresIn: "7d",
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
+
+export function verifyToken(token: string): User {
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    return decode as User;
+  } catch (error: ErrorType) {
+    serverLog(error);
+    throw new Error("Invalid or expired token");
+  }
+}
