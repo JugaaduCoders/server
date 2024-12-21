37ffd05840c2facc9713bb1fc7c95de33c9e7aaa
Prakash Rathour <prakashkumarrathour247@gmail.com>
2024-12-21 22:56:18 +0530

    feat: creates a function to generate template for modules

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,9 @@ export type GenericObject = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
 };
+
+export enum UserRoleEnum {
+  PARTICIPANT = 'participant',
+  USER = 'user',
+  ADMIN = 'admin',
+}
